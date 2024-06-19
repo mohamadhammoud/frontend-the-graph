d70a3eaaef352ce60f43285bfd9ae74847a10baa
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2024-06-19 21:24:49 +0300

    🎉 started project

---
+++ constants/queries.qgl.ts
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+export const pairSymbolsQuery = gql`
+  {
+    pairs {
+      token0 {
+        symbol
+      }
+      token1 {
+        symbol
+      }
+    }
+  }
+`;
